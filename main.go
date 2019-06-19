5b967ae05c1828472b7d1d331a52ceea3840915c
vanshilshah <mr.vanshilshah@gmail.com>
2019-06-19 16:09:00 -0400

    added basic react layout

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
+
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	// Setup route group for the API
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+
+	// Start and run the server
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+	router.Run(":" + port)
+}
---
+++ views/js/app.jsx
@@ -0,0 +1,7 @@
+class App extends React.Component {
+    render() {
+      return (<p>Hello from React!</p>);
+    }
+  }
+
+ReactDOM.render(<App/>, document.getElementById('app'));
