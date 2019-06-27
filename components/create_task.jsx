9cbb91ae4877807187dfca3b5527a37ee6326e41
vanshilshah <mr.vanshilshah@gmail.com>
2019-06-27 11:31:25 -0400

    added basic form fields for create task

--- components/create_task.jsx
+++ components/create_task.jsx
@@ -1,11 +1,15 @@
-import { Paper } from '@material-ui/core';
+import MomentUtils from '@date-io/moment';
+import { Paper, TextField } from '@material-ui/core';
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import moment from 'moment';
 import React from 'react';
 
 export default class CreateTask extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        active: this.props.active
+        active: this.props.active,
+        selectedDate: moment()
       }
     }
 
@@ -13,10 +17,45 @@ export default class CreateTask extends React.Component {
       this.setState({active: !this.state.active});
     }
 
+    handleDateChange = date => {
+      this.setState({selectedDate: date})
+    }
+
     render() {
-        const classes = 'createTask' + (this.state.active? ' createTaskActive':'');
-        return (<Paper className={classes}>
-        </Paper>);
+      const classes = 'createTask' + (this.state.active? ' createTaskActive':'');
+      return (
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+      <Paper className={classes}>
+        <div className='createTaskHeader'>
+          New task
+        </div>
+        <div className='createTaskContent'>
+          <TextField
+            id="taskName"
+            label="Name"/>
+          <TextField
+            id="name"
+            label="Name"/>
+          <DatePicker
+            id="deadline"
+            label="Deadline"
+            disablePast
+            allowKeyboardControl
+            value={this.state.selectedDate}
+            onChange={this.handleDateChange}
+            minDate={new Date()}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+          <TextField
+            id="name"
+            label="Name"/>
+          <TextField
+            id="name"
+            label="Name"/>
+        </div>
+      </Paper></MuiPickersUtilsProvider>);
             
     }
   }
